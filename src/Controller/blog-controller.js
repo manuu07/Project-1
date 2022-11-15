5d3bb2091c47aa68202b8903fcbdeeaa9dc99a0d
Saurabhsawant8 <sawantsaurabh2399@gmail.com>
2022-11-15 14:05:34 +0530

    delete blogs by query

--- src/Controller/blog-controller.js
+++ src/Controller/blog-controller.js
@@ -1,5 +1,6 @@
 const blogModel = require('../models/blogModel')
 const authorModel = require('../models/authorModel')
+const moment = require('moment')
 
 
  const createBlog=async function (req,res){
@@ -27,6 +28,7 @@ const getBlogs = async function(req,res){
          const getAllBlogs = await blogModel.find({isPublished : true , isDeleted : false })
           return  res.status(200).send({status : true , message : getAllBlogs})
         }
+    
          const getBlog = await blogModel.find({$or:[{category : category} , {subcategory : subcategory} ,{tags:tag }, {_id : authorId}]}).populate('authorId')
 console.log(getBlog)
         if(getBlog.length==0){
@@ -42,4 +44,42 @@ console.log(getBlog)
     }
 }
 
+
+const deleteblogs=async function(req,res){
+    try{
+  
+    const {category , subcategory , tag , authorId}  = req.query
+    if(!category && !subcategory && !tag && !authorId ){
+      return  res.status(400).send({status : true , message : 'query does not exist'})
+    }
+
+    const getBlog = await blogModel.updateMany({$or:[{category : category} , {subcategory : subcategory} ,{tags:tag }, {_id : authorId}]})
+    const result = getBlog.filter(a=>{
+        if(a.isDeleted == false ){
+            return a
+        }
+    })
+    if (result.length==0){
+        return res.status(404).send({status : false , msg : 'already deleted'})
+    }else {
+        result.forEach(a=>{
+            a.isDeleted = true 
+            a.deletedAt = moment().format()
+        })
+    }
+result.save()
+res.status(200).send({status :true , msg : 'Deleted successfully ' })
+
+    // const data=await blogModel.find()
+    // if(data.isDeleted==false){
+    // // const data1=await blogModel.updateMany({$or:[{category : category},{subcategory : subcategory} ,{tags:tags },{_id : authorId},{unpublished:unpublished}]},{$set:{isDeleted:true}},{new:true})
+    // res.status(200).send({status :true , data : data1 })
+    // }else{
+    //     res.status(404).send({status : false , msg : "user does not exist"})
+    // }
+} catch (error) {
+    res.status(500).send({status : false , msg : error.message})
+}
+}
 module.exports.getBlogs = getBlogs
+module.exports.deleteblogs = deleteblogs

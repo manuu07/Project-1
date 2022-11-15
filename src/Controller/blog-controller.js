e3a9bd68237fe0f094e12ddaccc06b4b93e649de
Shiv-Raj-Singh <mt932747@gmail.com>
2022-11-15 12:45:28 +0530

    done

--- src/Controller/blog-controller.js
+++ src/Controller/blog-controller.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const blogModel = require('../models/blogModel')
 const authorModel = require('../models/authorModel')
 
@@ -24,16 +23,18 @@ module.exports.createBlog = createBlog
 const getBlogs = async function(req,res){
     try {
         const {category , subcategory , tag , authorId}  = req.query
-        const getBlog = await blogModel.find({$or:[{category : category} , {subcategory : subcategory} ,{tags:tag }, {_id : authorId}]}).populate('authorId')
-        console.log(getBlog)
+        if(!category && !subcategory && !tag && !authorId ){
+         const getAllBlogs = await blogModel.find({isPublished : true , isDeleted : false })
+          return  res.status(200).send({status : true , message : getAllBlogs})
+        }
+         const getBlog = await blogModel.find({$or:[{category : category} , {subcategory : subcategory} ,{tags:tag }, {_id : authorId}]}).populate('authorId')
+console.log(getBlog)
         if(getBlog.length==0){
             return  res.status(404).send({status : false , message : 'blog not found'})
-        }else
-        if ( getBlog.isPublished !== true && getBlog.isDeleted == false ){
-            res.status(400).send({status : false , message : 'blog is not published or maybe deleted '})
-       
-        }else{
+        }else if ( getBlog.isPublished == true && getBlog.isDeleted == false ){
             return  res.status(200).send({status :true , data : getBlog })
+        }else{
+            res.status(400).send({status : false , message : 'blog is not published or maybe deleted '})
         }
         
     } catch (error) {
